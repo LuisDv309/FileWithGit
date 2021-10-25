c464a6462acef62d91cc02a6a83427dec279e7c2
Luis Perez Pena <270161235@yoobeestudent.ac.nz>
2021-10-26 09:53:27 +1300

    Filestest

---
+++ FileswithGithub.cpp
@@ -0,0 +1,22 @@
+// FileswithGithub.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+struct Jacket
+{
+    string Brand;
+    string Material;
+    string color;
+    char Size;
+    int Price;
+
+
+};
+int main()
+{
+  
+}
+
